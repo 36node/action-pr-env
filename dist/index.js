6b1e63e6a6357900fc95f90733f3b2db75a52a06
zzswang <zzswang@gmail.com>
2024-01-13 15:29:41 +0800

    fix: prettier check

--- dist/index.js
+++ dist/index.js
@@ -2722,6 +2722,33 @@ exports["default"] = _default;
 
 /***/ }),
 
+/***/ 996:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.extract = void 0;
+/**
+ * Extract env from text.
+ *
+ * @param text input string.
+ * @returns {{[key:string]: string}} Resolves with a map of environment variables.
+ */
+function extract(text = '') {
+    const regex = new RegExp(/\+env:(.*)=(.*)/g);
+    const env = {};
+    let results;
+    while ((results = regex.exec(text)) !== null) {
+        env[results[1]] = results[2];
+    }
+    return env;
+}
+exports.extract = extract;
+
+
+/***/ }),
+
 /***/ 399:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -2753,22 +2780,24 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(259);
+const env_1 = __nccwpck_require__(996);
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
+        const text = core.getInput('text');
+        const env = (0, env_1.extract)(text);
         // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
+        const output = JSON.stringify(env, null, 2);
+        core.debug(`env ${output}`);
         // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        core.setOutput('env', output);
+        // write to $GITHUB_ENV
+        for (const [key, value] of Object.entries(env)) {
+            core.exportVariable(key, value);
+        }
     }
     catch (error) {
         // Fail the workflow run if an error occurs
@@ -2777,31 +2806,6 @@ async function run() {
     }
 }
 exports.run = run;
-
-
-/***/ }),
-
-/***/ 259:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise(resolve => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
